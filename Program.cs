4ff033a692e83675cf62f2e8ee8fedec6a4dca3e
Phùng Tuấn Huy <huythelep@gmail.com>
2025-12-12 22:42:31 +0700

    Reapply "oki"
    This reverts commit bba17bbc28508cc09ac4e07c6833d027c106287a.

--- Program.cs
+++ Program.cs
@@ -23,7 +23,7 @@ namespace QuanLyTrangTrai
             }
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new GiaoDien());
+            Application.Run(new Form1());
         }
         [System.Runtime.InteropServices.DllImport("user32.dll")]
         private static extern bool SetProcessDPIAware();
